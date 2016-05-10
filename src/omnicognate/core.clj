8f523da6952f99390e478e077930a0e878f52b31
Neel Upadhyaya <neel.upadhyaya@eckoh.com>
2016-05-10 17:54:41 +0100

    Basic operation of commit and rebase with [op e a v] txes

--- src/omnicognate/core.clj
+++ src/omnicognate/core.clj
@@ -1,5 +1,6 @@
 (ns omnicognate.core
-  (:require [taoensso.truss :as truss :refer [have have! have?]]))
+  (:require [taoensso.truss :as truss :refer [have have! have?]]
+            [clojure.set :as set]))
 
 (defprotocol TxLog
   (-commit [log tx tempids])
@@ -26,16 +27,72 @@
 (defn heads [log]
   (-heads (have tx-log? log)))
 
-(defrecord DatomTxLog [log refs]
+(defn references [refs tx]
+  (into #{}
+        (mapcat (fn [datom]
+                  (let [[op e a v] datom]
+                    (if (refs a)
+                      [e v]
+                      [e])))
+                tx)))
+
+(defn tx-ready? [{:keys [tempids deps]}]
+  (empty? deps))
+
+(defn ready-txes [tx-info log]
+  (filter (fn [tx]
+            (tx-ready? (tx-info tx)))
+          log))
+
+(defn update-ids [tx old-id->new-id refs]
+  (mapv (fn [datom]
+          (let [[op e a v] datom
+                e (or (old-id->new-id e) e)
+                v (if (refs a)
+                    (or (old-id->new-id v) v)
+                    v)]
+            [op e a v]))
+        tx))
+
+(defrecord DatomTxLog [refs log tx-info ids]
   TxLog
   (-commit [self tx tempids]
-    self)
+    (let [deps (set/intersection ids (references refs tx))]
+      (-> self
+          (update :log conj tx)
+          (update :tx-info assoc tx {:tempids tempids :deps deps})
+          (update :ids into (vals tempids)))))
   (-rebase [self tx tempids]
-    self)
+    (if-let [tx-info' (tx-info tx)]
+      (let [tempids' (:tempids tx-info')
+            new-log (remove #{tx} log)
+            old-ids (set (vals tempids'))
+            new-ids (set (vals tempids))
+            old-id->new-id (into {} (keep (fn [[tempid new-id]]
+                                            (when-let [old-id (tempids' tempid)]
+                                              [old-id new-id]))
+                                          tempids))
+            new-tx-info (into {} (map (fn [tx']
+                                        (let [info (update (tx-info tx')
+                                                           :deps
+                                                           set/difference
+                                                           old-ids)
+                                              tx' (update-ids tx' old-id->new-id refs)]
+                                          [tx' info]))
+                                      new-log))]
+        (-> self
+            (update :ids set/difference old-ids)
+            (update :ids set/union new-ids)
+            (assoc :tx-info new-tx-info)
+            (assoc :log (keys new-tx-info))))
+      self))
   (-head [self]
-    nil)
+    (first (ready-txes tx-info log)))
   (-heads [self]
-    nil))
+    (into #{} (ready-txes tx-info log))))
 
 (defn datom-tx-log [refs]
-  (->DatomTxLog [] (have set? refs)))
+  (->DatomTxLog (have set? refs)
+                []
+                {}
+                #{}))
--- test/omnicognate/core_test.clj
+++ test/omnicognate/core_test.clj
@@ -3,26 +3,27 @@
             [omnicognate.core :refer :all]
             [datomic.api :as d]))
 
-(def empty-log (datom-tx-log #{:in :employs}))
+(defn empty-log []
+  (datom-tx-log #{:in :knows}))
 
 (def authur
-  {:tx [[-1 :name "Authur"]]
+  {:tx [[:add -1 :name "Authur"]]
    :ids {-1 1}})
 
 (def ford-and-zaphod
-  {:tx [[-1 :name "Ford"]
-        [-2 :name "Zaphod"]]
+  {:tx [[:add -1 :name "Ford"]
+        [:add -2 :name "Zaphod"]]
    :ids {-1 2
          -2 3}})
 
 (defn heart-of-gold [zaphod]
-  {:tx [[-1 :ship "Heart of Gold"]
-        [zaphod :in -1]]
+  {:tx [[:add -1 :ship "Heart of Gold"]
+        [:add zaphod :in -1]]
    :ids {-1 4}})
 
 (defn trillian [authur]
-  {:tx [[-1 :name "Trillian"]
-        [-1 :knows authur]]
+  {:tx [[:add -1 :name "Trillian"]
+        [:add -1 :knows authur]]
    :ids {-1 5}})
 
 (def log-data
@@ -31,43 +32,45 @@
    (heart-of-gold 3)
    (trillian 1)])
 
-(def log
+(defn log []
   (reduce (fn [log {:keys [tx ids]}]
             (commit log tx ids))
-          empty-log
+          (empty-log)
           log-data))
 
 (deftest the-empty-log
-  (testing "The empty log is a log"
-    (is (tx-log? empty-log)))
-  (testing "There is nothing at the head"
-    (is (= nil (head empty-log))))
-  (testing "The set of heads is also empty"
-    (is (= nil (heads empty-log)))))
+  (let [empty-log (empty-log)]
+    (testing "The empty log is a log"
+      (is (tx-log? empty-log)))
+    (testing "There is nothing at the head"
+      (is (= nil (head empty-log))))
+    (testing "The set of heads is also empty"
+      (is (= #{} (heads empty-log))))))
 
 (deftest log-head
-  (testing "The head is the first commit"
-    (is (= (:tx authur) (head log))))
-  (testing "Both the first ans second commits are at the head"
-    (is (= #{(:tx authur)
-             (:tx ford-and-zaphod)}
-           (heads log)))))
+  (let [log (log)]
+    (testing "The head is the first commit"
+      (is (= (:tx authur) (head log))))
+    (testing "Both the first and second commits are at the head"
+      (is (= #{(:tx authur)
+               (:tx ford-and-zaphod)}
+             (heads log))))))
 
 (deftest rebase-authur
-  (testing "Rebasing the first commit"
-    (let [tx (:tx authur)
-          new-log (rebase log tx {-1 100})]
-      (is (= (:tx ford-and-zaphod) (head new-log)))
-      (is (= #{(:tx ford-and-zaphod)
-               (:tx (trillian 100))}
-             (heads new-log))))))
+  (let [log (log)]
+    (testing "Rebasing the first commit"
+      (let [tx (:tx authur)
+            new-log (rebase log tx {-1 100})]
+        (is (= #{(:tx ford-and-zaphod)
+                 (:tx (trillian 100))}
+               (heads new-log)))))))
 
 (deftest rebase-authur-ford-and-zaphod
-  (testing "Rebasing the first commit"
-    (let [new-log (-> log
-                      (rebase (:tx authur) {-1 100})
-                      (rebase (:tx ford-and-zaphod) {-1 101 -2 102}))]
-      (is (= (:tx ford-and-zaphod) (head new-log)))
-      (is (= #{(heart-of-gold 102)
-               (trillian 100)}
-             (heads new-log))))))
+  (let [log (log)]
+    (testing "Rebasing the first commit"
+      (let [new-log (-> log
+                        (rebase (:tx authur) {-1 100})
+                        (rebase (:tx ford-and-zaphod) {-1 101 -2 102}))]
+        (is (= #{(:tx (heart-of-gold 102))
+                 (:tx (trillian 100))}
+               (heads new-log)))))))
