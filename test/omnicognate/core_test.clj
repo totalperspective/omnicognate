acd23b22442a796b36be643bd2130f3a21d3ab97
Neel Upadhyaya <neel.upadhyaya@eckoh.com>
2016-05-10 15:01:01 +0100

    Failing tests

--- test/omnicognate/core_test.clj
+++ test/omnicognate/core_test.clj
@@ -5,19 +5,31 @@
 
 (def empty-log (datom-tx-log #{:in :employs}))
 
+(def authur
+  {:tx [[-1 :name "Authur"]]
+   :ids {-1 1}})
+
+(def ford-and-zaphod
+  {:tx [[-1 :name "Ford"]
+        [-2 :name "Zaphod"]]
+   :ids {-1 2
+         -2 3}})
+
+(defn heart-of-gold [zaphod]
+  {:tx [[-1 :ship "Heart of Gold"]
+        [zaphod :in -1]]
+   :ids {-1 4}})
+
+(defn trillian [authur]
+  {:tx [[-1 :name "Trillian"]
+        [-1 :knows authur]]
+   :ids {-1 5}})
+
 (def log-data
-  [{:tx [[-1 :name "Authur"]]
-    :ids {-1 1}}
-   {:tx [[-1 :name "Ford"]
-         [-2 :name "Zaphod"]]
-    :ids {-1 2
-          -2 3}}
-   {:tx [[-1 :ship "Heart of Gold"]
-         [3 :in -1]]
-    :ids {-1 4}}
-   {:tx [[-1 :org "Megadodo Publications"]
-         [-1 :employs 2]]
-    :ids {-1 5}}])
+  [authur
+   ford-and-zaphod
+   (heart-of-gold 3)
+   (trillian 1)])
 
 (def log
   (reduce (fn [log {:keys [tx ids]}]
@@ -25,18 +37,37 @@
           empty-log
           log-data))
 
-(deftest empty-log
+(deftest the-empty-log
+  (testing "The empty log is a log"
+    (is (tx-log? empty-log)))
   (testing "There is nothing at the head"
-    (is (= nil (head empty-log)))
+    (is (= nil (head empty-log))))
+  (testing "The set of heads is also empty"
     (is (= nil (heads empty-log)))))
 
 (deftest log-head
   (testing "The head is the first commit"
-    (is (= (get-in log-data [0 :tx]) (head log))))
+    (is (= (:tx authur) (head log))))
   (testing "Both the first ans second commits are at the head"
-    (let [head-txes (into #{} (map :tx (take 2 log-data)))]
-      (is (= head-txes (heads log))))))
+    (is (= #{(:tx authur)
+             (:tx ford-and-zaphod)}
+           (heads log)))))
 
-;; First create a db and add some data
-;; Then stage some commits onto it
-;; Next push the commits onto the db
+(deftest rebase-authur
+  (testing "Rebasing the first commit"
+    (let [tx (:tx authur)
+          new-log (rebase log tx {-1 100})]
+      (is (= (:tx ford-and-zaphod) (head new-log)))
+      (is (= #{(:tx ford-and-zaphod)
+               (:tx (trillian 100))}
+             (heads new-log))))))
+
+(deftest rebase-authur-ford-and-zaphod
+  (testing "Rebasing the first commit"
+    (let [new-log (-> log
+                      (rebase (:tx authur) {-1 100})
+                      (rebase (:tx ford-and-zaphod) {-1 101 -2 102}))]
+      (is (= (:tx ford-and-zaphod) (head new-log)))
+      (is (= #{(heart-of-gold 102)
+               (trillian 100)}
+             (heads new-log))))))
