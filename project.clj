1e1e0e1061998e94e8283e60bb7d9983915bdf62
Bahul Neel Upadhyaya <bahulneel@gmail.com>
2016-05-17 15:51:08 +0100

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject omnicognate "0.1.2"
+(defproject omnicognate "0.1.3-SNAPSHOT"
   :description "Like git, but for datomic"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
